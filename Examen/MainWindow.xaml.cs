16bc15ac03bd7497334e1a1af7ddc65df286cc94
NeWetich <62741182+NeWetich@users.noreply.github.com>
2021-06-28 07:03:34 +0300

    Add files via upload

--- Examen/MainWindow.xaml.cs
+++ Examen/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Data.SqlClient;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,16 +20,31 @@ namespace Examen
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {       
+    {
+        DatabaseEntities db;
         public MainWindow()
         {
             InitializeComponent();
-            cashbox.Text = Convert.ToString(0);
+            db = new DatabaseEntities();
+            string summa = "SELECT Money FROM CashBox WHERE Id = 1";
+            int mon = 0;
+            using (SqlConnection thisConnection = new SqlConnection("data source=(LocalDB)\\MSSQLLocalDB;attachdbfilename=|DataDirectory|\\Database.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;"))
+            {
+                using (SqlCommand cmdCount = new SqlCommand(summa, thisConnection))
+                {
+                    thisConnection.Open();
+                    cashbox.Text = Convert.ToString(mon);
+                    thisConnection.Close();
+                }
+            }
         }
 
         private void Insert_Click(object sender, RoutedEventArgs e)
         {
-            cashbox.Text = sum.Text;
+            int x = Convert.ToInt32(sum.Text);
+            int y = Convert.ToInt32(cashbox.Text);
+            int z = x + y;
+            cashbox.Text = z.ToString();
         }
     }
 }
